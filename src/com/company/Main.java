a68c015b53ab66b38a8aa742b151235ecd460cee
DorottyaKrisztics <dorka713@gmail.com>
2017-08-20 20:38:37 +0200

    generate different names of motorcycle objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -46,6 +46,7 @@ public class Main {
 
     public static void main(String[] args) {
         Motorcycle motorcycle1 = new Motorcycle();
+        Motorcycle motorcycle2 = new Motorcycle();
 
         motorcycle1.moveForAnHour(isRaining());
         motorcycle1.moveForAnHour(true);
--- src/com/company/Motorcycle.java
+++ src/com/company/Motorcycle.java
@@ -5,9 +5,15 @@ import java.util.Random;
 
 public class Motorcycle {
 
+    Motorcycle(){
+        setSlowerSpeed(false);
+        createMotorcycleName();
+    }
+
     // speed is 100km/h. If rains, travels with 5-50km/h slower (randomly).
     int motorcycleSpeed = 100;
     int motorSpeedMin = 5;
+
     int motorSpeedMax = 50;
 
     void setSlowerSpeed(boolean rainyWeather) {
@@ -24,11 +30,23 @@ public class Motorcycle {
         System.out.println(rainyWeather);
     }
 
-    //static nameNumber // The number of the instance created. Used for its name.
-    //name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
+
+    //static nameNumber: The number of the instance created. Used for its name
+    //name: are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique
+    static int nameNumber = 1;
+
+    String motorcycleName;
+
+    void createMotorcycleName() {
+
+        motorcycleName = "Motorcycle " + nameNumber;
+        nameNumber++;
+        System.out.println(motorcycleName);
+    }
 
 
     //distanceTraveled, moveForAnHour()
+
     int distanceTraveled = 0;
 
     void moveForAnHour(boolean rainyWeather) {
@@ -39,9 +57,5 @@ public class Motorcycle {
         System.out.println(distanceTraveled);
     }
 
-
-    Motorcycle(){
-        setSlowerSpeed(Main.isRaining());
-    }
 }
 
